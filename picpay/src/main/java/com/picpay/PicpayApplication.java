5a4e2e2d92f3607f56f865e982819cb03e43a70d
Leonardo <Leonardo@DESKTOP-5NK7MJ3>
2023-08-26 17:24:55 -0300

    inicial commit

---
+++ picpay/src/main/java/com/picpay/PicpayApplication.java
@@ -0,0 +1,18 @@
+package com.picpay;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayApplication.class, args);
+		
+		
+		
+		
+	}
+
+}
