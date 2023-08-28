6acf2c64d10f71299a4a3d0a86293ee0181d4649
Leonardo <Leonardo@DESKTOP-5NK7MJ3>
2023-08-28 18:42:51 -0300

    created notificationService

---
+++ picpay/src/main/java/com/picpay/services/NotificationService.java
@@ -0,0 +1,34 @@
+package com.picpay.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.picpay.domain.user.User;
+import com.picpay.dtos.NotificationDTO;
+
+@Service
+public class NotificationService {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	
+	public void sendNotification(User  user, String message) throws Exception {
+		
+		String email = user.getEmail();
+		NotificationDTO notificationRequest = new NotificationDTO(email, message);
+		
+		ResponseEntity<String> notificationResponse = restTemplate.postForEntity("http://o4d9z.mocklab.io/notify", notificationRequest, String.class);
+		
+		if(!(notificationResponse.getStatusCode() == HttpStatus.OK)) {
+			System.out.println("erro ao enviar notificação");
+			throw new Exception("serviço de notificação está fora do ar");
+		}
+		
+	}
+
+}
