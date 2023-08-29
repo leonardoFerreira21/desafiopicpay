5aa842c201c9f21bea9b8f757e6ac5e17728a247
Leonardo <Leonardo@DESKTOP-5NK7MJ3>
2023-08-28 23:29:07 -0300

    minor changes

--- picpay/src/main/java/com/picpay/domain/user/User.java
+++ picpay/src/main/java/com/picpay/domain/user/User.java
@@ -17,6 +17,7 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity(name = "users")
@@ -24,6 +25,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 
 public class User {
